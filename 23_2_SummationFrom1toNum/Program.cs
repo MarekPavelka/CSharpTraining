17ecedb010cf549f08f4a4f2e4297b8b95a78586
Marek Pavelka <marek.pavelka@gmail.com>
2019-02-23 18:16:55 +0100

    Grasshopper

---
+++ 23_2_SummationFrom1toNum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _23_2_SummationFrom1toNum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = 8;
+            int sum = 0;
+            for (var start = 1; start <= x; start++)
+            {
+                Console.Write("{0}", start);
+                sum = sum + start;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sucet tychto cisiel je {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
