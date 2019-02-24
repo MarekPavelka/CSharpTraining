288fe6fb8b209c0a5c31c4a85dd2e3331dea4a10
Vladimir Pavelka <pavelka.vlado@gmail.com>
2019-02-24 15:18:36 +0100

    Smiley count alternative solution

--- 24_2SmileyCounter2/Program.cs
+++ 24_2SmileyCounter2/Program.cs
@@ -7,18 +7,20 @@ using System.Threading.Tasks;
 
 namespace _24_2SmileyCounter2
 {
+    using System.Runtime.InteropServices;
+
     class Program
     {
         static void Main()
         {
-            var listSmajlikov = new List<string>(){""};
+            var listSmajlikov = new List<string>() { "" };
             Console.WriteLine("Smajliky ktore poznam su :) :D ;) ;D :-) ;-) :-D ;-D");
             Console.WriteLine();
             Console.WriteLine("Postupne zadavaj smajlikov, pre ukoncenie napis koniec:");
 
             for (int counter = 0; listSmajlikov[counter] != "koniec"; counter++)
             {
-                Console.WriteLine("Zadaj {0}. smajlika a potvrd enterom", counter+1);
+                Console.WriteLine("Zadaj {0}. smajlika a potvrd enterom", counter + 1);
                 listSmajlikov.Add(Console.ReadLine());
             }
             Console.WriteLine("Naratal som {0} smajlikov ktore poznam", CountSmileys(listSmajlikov));
@@ -37,6 +39,45 @@ namespace _24_2SmileyCounter2
                 }
             }
             return count;
+        }
+
+        /// <summary>
+        /// Riesenie 2
+        /// </summary>
+        public static int SmileysCount(List<string> smileys)
+        {
+            var count = 0;
+            foreach (string smiley in smileys)
+            {
+                var isSmiley = IsSmiley(smiley);
+                if (isSmiley) count++;
+            }
+
+            return count;
+        }
+
+        public static bool IsSmiley(string smiley)
+        {
+            // Smajliky ktore poznam su :) :D ;) ;D :-) ;-) :-D ;-D
+            if (!smiley.StartsWith(":") && !smiley.StartsWith(";"))
+            {
+                return false;
+            }
+
+            var hasCorrectEnding = !smiley.EndsWith(")") && !smiley.EndsWith("D");
+            if (hasCorrectEnding)
+            {
+                return false;
+            }
+
+            var hasNose = smiley.Length > 2;
+            if (!hasNose)
+            {
+                return true;
+            }
+
+            char nose = smiley[1];
+            return nose == '-' || nose == '~';
         }
     }
 }
